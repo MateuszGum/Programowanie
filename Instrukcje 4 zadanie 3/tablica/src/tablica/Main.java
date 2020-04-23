bc159d02cefb3e78132b237f782676a60fb09def
MateuszGum <43993292+MateuszGum@users.noreply.github.com>
2020-04-23 20:03:29 +0200

    Add files via upload

---
+++ Instrukcje 4 zadanie 3/tablica/src/tablica/Main.java
@@ -0,0 +1,28 @@
+package tablica;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int[] tab = new int [200];
+		for(int i=0;i<200;i++){
+			tab[i]=0;
+		}
+		
+		System.out.println("podaj który element tablicy chcesz wyœwietliæ");
+		
+			Scanner scan = new Scanner(System.in);
+		int nowy = Integer.parseInt(scan.next());
+		
+		
+		try {
+			System.out.println(tab[nowy]);
+			
+		}
+		catch(IndexOutOfBoundsException e){
+			System.out.println("tablica nie ma takiego elementu");
+		}
+	}
+}
